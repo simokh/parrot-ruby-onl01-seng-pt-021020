8040b5fbbc4db52fcd0521f9787cbf0816acd466
Simo Khouili <m.simokhouili@gmail.com>
2020-02-13 04:56:32 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -3,19 +3,3 @@ def parrot(phrase = 'Squawk!')
 puts phrase
 phrase
 end
-
-
-# def parrot(phrase="Squawk!")
-# puts"Squawk!"
-# end
-# parrot("Squawk!")
-#
-# def parrot(phrase="Squawk!")
-#  puts"Squawk!"
-# end
-# parrot
-#
-# def parrot(phrase="Squawk!")
-# return "Pretty bird!"
-# end
-# parrot
