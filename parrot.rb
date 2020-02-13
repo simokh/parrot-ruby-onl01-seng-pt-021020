b50bb28623f17a0d697d605d8cc98bac1a60ec1d
Simo Khouili <m.simokhouili@gmail.com>
2020-02-13 02:59:07 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,20 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+
+def parrot(phrase="Squawk!")
+puts phrase
+end
+parrot
+
+# def parrot(phrase="Squawk!")
+# puts"Squawk!"
+# end
+# parrot("Squawk!")
+#
+# def parrot(phrase="Squawk!")
+#  puts"Squawk!"
+# end
+# parrot
+#
+# def parrot(phrase="Squawk!")
+# return "Pretty bird!"
+# end
+# parrot
